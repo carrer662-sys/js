c510fae2611058aaeb236508a03b7078afd328fc
carrer662-sys <carrer662@gmail.com>
2025-09-10 12:49:02 +0530

    10-9-2025

---
+++ jssix/script.js
@@ -0,0 +1,90 @@
+// let array = ["apple", "orange", "mango", "banana", "cherry", "blueberry"];
+// let res = array.shift();
+// let res = array.slice(2,4);
+// array.splice(2,1,"peach");
+// console.log(res);
+// array.push("mango");
+// console.log(array);
+
+
+
+
+// function myFunction(msg, n) {
+//     // console.log("Hello!");
+//     console.log(msg/n);
+
+// }
+// myFunction("Hi", 5);
+
+// function => 2 mumbers, sum
+
+// function sum(a, b) {
+//     s = a + b;
+//     console.log("before return");
+//     return s;
+//     console.log("after return");
+
+// }
+// let val = sum(3, 4)
+// console.log(val);
+
+// function sum(x, y) {
+//     console.log(x+y);
+
+// }
+// sum(4, 4);
+
+// const sumArrow = (a, b) => {
+//     console.log(a + b);
+
+// }
+// sumArrow(5, 5);
+
+// function mul(a, b) {
+//     return a * b;
+// }
+// let res = mul(2, 2);
+// console.log(res);
+
+// const mulArrow = (x, y) => {
+//     return x * y;
+// }
+// let resArrow = mulArrow(3, 3);
+// console.log(resArrow);
+
+// const res = () => console.log("hello");
+// res();
+
+// function countVowels(str) {
+//   let count = 0;
+//   for (const char of str) {
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+// countVowels("hello");
+
+// const countVow = (str) => {
+//   let count = 0;
+//   for (const char of str) {
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// };
+// countVow("hello");
---
+++ tasks/script.js
@@ -0,0 +1,15 @@
+// const panels = document.querySelectorAll(".panel");
+
+// panels.forEach((panel) => {
+//     panel.addEventListener("click", () => {
+//         removeActivePanel();
+//         panel.classList.add("active");
+//     });
+// });
+
+// const removeActivePanel = () => {
+//     panels.forEach((panel) => {
+//         panel.classList.remove("active");
+//     });
+// };
+
